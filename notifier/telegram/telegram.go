e877ebef7596a895dfa902611d5da852308edc4e
RoCry <crysheen@gmail.com>
2017-11-23 14:47:49 +0800

    telegram notifier

---
+++ notifier/telegram/telegram.go
@@ -0,0 +1,79 @@
+package telegram
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Notifier struct {
+	Token  string
+	ChatID int
+}
+
+func (n *Notifier) Notify(text string, images []string) {
+	imageCount := len(images)
+
+	if imageCount > 1 {
+		mediaPhotos := make([]map[string]string, imageCount)
+		for i := 0; i < imageCount; i++ {
+			mediaPhotos[i] = map[string]string{
+				"type":    "photo",
+				"media":   images[i],
+				"caption": text,
+			}
+		}
+		n.send("sendMediaGroup", map[string]interface{}{
+			"caption": text,
+			"chat_id": n.ChatID,
+			"media":   mediaPhotos,
+		})
+	} else if imageCount == 1 {
+		n.send("sendPhoto", map[string]interface{}{
+			"caption": text,
+			"chat_id": n.ChatID,
+			"photo":   images[0],
+		})
+	} else {
+		n.send("sendMessage", map[string]interface{}{
+			"text":       text,
+			"chat_id":    n.ChatID,
+			"parse_mode": "Markdown",
+		})
+	}
+}
+
+func (n *Notifier) send(method string, values interface{}) error {
+	jsonValue, err := json.Marshal(values)
+	if err != nil {
+		return err
+	}
+	body := bytes.NewBuffer(jsonValue)
+
+	// Create client
+	client := &http.Client{}
+
+	// Create request
+	path := fmt.Sprintf("https://api.telegram.org/bot%s/%s", n.Token, method)
+	req, err := http.NewRequest("POST", path, body)
+	if err != nil {
+		return err
+	}
+
+	// Headers
+	req.Header.Add("Content-Type", "application/json")
+
+	err = req.ParseForm()
+	if err != nil {
+		return err
+	}
+
+	// Fetch Request
+	_, err = client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
